6360a7662d50b4a135dfd484b9b0de8fbe8997a5
Rishabh Sharma <103810649+rishabh-xv@users.noreply.github.com>
2023-05-17 11:00:21 +0530

    Update rr.c

--- rr.c
+++ rr.c
@@ -1,46 +1,67 @@
-#include <stdio.h>
-void findWaitingTime(int processes[], int n, int bt[], int wt[])
-{
-wt[0] = 0;
-for (int i = 1; i < n; i++)
-{
-wt[i] = bt[i-1] + wt[i-1];
-}
-}
-void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[])
-{
-// bt[i] + wt[i]
-for (int i = 0; i < n; i++)
-{
-tat[i] = bt[i] + wt[i];
-}
-}
-void findavgTime(int processes[], int n, int bt[])
-{
-int wt[n], tat[n], total_wt = 0, total_tat = 0;
-findWaitingTime(processes, n, bt, wt);
-findTurnAroundTime(processes, n, bt, wt, tat);
-printf("Processes Burst time Waiting time Turn around time\n");
-for (int i = 0; i < n; i++)
-{
-total_wt = total_wt + wt[i];
-total_tat = total_tat + tat[i];
-printf(" %d ",(i+1));
-printf(" %d ", bt[i]);
-printf(" %d",wt[i]);
-printf(" %d\n",tat[i]);
-}
-float s=(float)total_wt / (float)n;
-float t=(float)total_tat / (float)n;
-printf("Average waiting time = %f",s);
-printf("\n");
-printf("Average turn around time = %f ",t);
-}
-int main()
-{
-int processes[] = { 1, 2, 3};
-int n = sizeof processes / sizeof processes[0];
-int burst_time[] = {10, 5, 8};
-findavgTime(processes, n, burst_time);
-return 0;
+#include<stdio.h>  
+  
+void main()  
+{  
+    // initlialize the variable name  
+    int i, NOP, sum=0,count=0, y, quant, wt=0, tat=0, at[10], bt[10], temp[10];  
+    float avg_wt, avg_tat;  
+    printf(" Total number of process in the system: ");  
+    scanf("%d", &NOP);  
+    y = NOP; // Assign the number of process to variable y  
+  
+// Use for loop to enter the details of the process like Arrival time and the Burst Time  
+for(i=0; i<NOP; i++)  
+{  
+printf("\n Enter the Arrival and Burst time of the Process[%d]\n", i+1);  
+printf(" Arrival time is: \t");  // Accept arrival time  
+scanf("%d", &at[i]);  
+printf(" \nBurst time is: \t"); // Accept the Burst time  
+scanf("%d", &bt[i]);  
+temp[i] = bt[i]; // store the burst time in temp array  
+}  
+// Accept the Time qunat  
+printf("Enter the Time Quantum for the process: \t");  
+scanf("%d", &quant);  
+// Display the process No, burst time, Turn Around Time and the waiting time  
+printf("\n Process No \t\t Burst Time \t\t TAT \t\t Waiting Time ");  
+for(sum=0, i = 0; y!=0; )  
+{  
+if(temp[i] <= quant && temp[i] > 0) // define the conditions   
+{  
+    sum = sum + temp[i];  
+    temp[i] = 0;  
+    count=1;  
+    }     
+    else if(temp[i] > 0)  
+    {  
+        temp[i] = temp[i] - quant;  
+        sum = sum + quant;    
+    }  
+    if(temp[i]==0 && count==1)  
+    {  
+        y--; //decrement the process no.  
+        printf("\nProcess No[%d] \t\t %d\t\t\t\t %d\t\t\t %d", i+1, bt[i], sum-at[i], sum-at[i]-bt[i]);  
+        wt = wt+sum-at[i]-bt[i];  
+        tat = tat+sum-at[i];  
+        count =0;     
+    }  
+    if(i==NOP-1)  
+    {  
+        i=0;  
+    }  
+    else if(at[i+1]<=sum)  
+    {  
+        i++;  
+    }  
+    else  
+    {  
+        i=0;  
+    }  
+}  
+// represents the average waiting time and Turn Around time  
+avg_wt = wt * 1.0/NOP;  
+avg_tat = tat * 1.0/NOP;  
+printf("\n Average Turn Around Time: \t%f", avg_wt);  
+printf("\n Average Waiting Time: \t%f", avg_tat);  
+//getch();  
 }
